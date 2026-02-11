45bec68435a9db93a9f5bb14484cfb2b2c9b1d59
Arash <eshkevar.arash1387@gmail.com>
2026-02-12 01:11:32 +0330

    First Commit

---
+++ data.js
@@ -0,0 +1,39 @@
+const users = [
+  {
+    id: 1,
+    username: "amin",
+    age: 24,
+    password: "amin121212",
+    email: "amin@gmail.com",
+  },
+  {
+    id: 2,
+    username: "amir",
+    age: 25,
+    password: "amir121212",
+    email: "amir@gmail.com",
+  },
+  {
+    id: 3,
+    username: "ali",
+    age: 18,
+    password: "ali121212",
+    email: "ali@gmail.com",
+  },
+  {
+    id: 4,
+    username: "mmd",
+    age: 25,
+    password: "mmd121212",
+    email: "mmd@gmail.com",
+  },
+  {
+    id: 5,
+    username: "babak",
+    age: 21,
+    password: "babak121212",
+    email: "babak@gmail.com",
+  },
+];
+
+export default users;
